8c25804fb6191372f56db70407cbfb698a0deacb
leo <leo493852107@163.com>
2022-07-03 19:00:44 +0800

    职称管理删除

--- src/components/sys/basic/JobLevelMana.vue
+++ src/components/sys/basic/JobLevelMana.vue
@@ -13,12 +13,20 @@
       </el-select>
       <el-button size="small" style="margin-left: 8px" type="primary" icon="el-icon-plus" @click="addJobLevel">添加
       </el-button>
+      <el-button size="small" style="margin-left: 10px" type="danger" :disabled="this.multipleSelection.length===0" @click="deleteBatch">
+        批量删除
+      </el-button>
       <el-table
           :data="jobLevelTable"
           stripe
           border
           size="small"
-          style="width: 70%; margin-top: 10px">
+          @selection-change="handleSelectionChange"
+          style="width: 100%; margin-top: 10px">
+        <el-table-column
+            type="selection"
+            width="55">
+        </el-table-column>
         <el-table-column
             prop="id"
             label="编号"
@@ -132,13 +140,39 @@ export default {
         '员级',
       ],
       jobLevelTable: [],
-      dialogVisible: false
+      dialogVisible: false,
+      multipleSelection: []
     }
   },
   mounted() {
     this.getAllData();
   },
   methods: {
+    deleteBatch(){
+      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let ids = '?';
+        this.multipleSelection.forEach(item => {
+          ids += 'ids=' + item.id+'&';
+        })
+        this.deleteRequest('/system/basic/joblevel/' + ids).then(response => {
+          if (response) {
+            this.getAllData();
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
     doUpdate() {
       this.putRequest('/system/basic/joblevel/', this.updateJobLevel).then(response => {
         if (response) {
