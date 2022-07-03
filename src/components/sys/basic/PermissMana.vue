416d5e6dcebf7f0c7469f9c3af595ce962094025
leo <leo493852107@163.com>
2022-07-03 21:05:30 +0800

    前端权限组树形菜单展示

--- src/components/sys/basic/PermissMana.vue
+++ src/components/sys/basic/PermissMana.vue
@@ -1,13 +1,85 @@
 <template>
-
+  <div>
+    <div class="permissManaTool">
+      <el-input size="small" placeholder="请输入英文名称" v-model="role.name">
+        <template slot="prepend">ROLE_</template>
+      </el-input>
+      <el-input size="small" v-model="role.nameZh" placeholder="请输入中文名称"></el-input>
+      <el-button size="small" type="primary" icon="el-icon-plus">添加角色</el-button>
+    </div>
+    <div class="permissManaMain">
+      <el-collapse accordion @change="changeCollapse">
+        <el-collapse-item :title="r.nameZh" :name="r.id" v-for="(r, index) in roles" :key="index">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>可访问资源</span>
+              <el-button style="float: right; padding: 3px 0; color: red" type="text" icon="el-icon-delete"></el-button>
+            </div>
+            <el-tree show-checkbox :data="allMenus" :props="defaultProps"></el-tree>
+          </el-card>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PermissMana"
+  name: "PermissMana",
+  data() {
+    return {
+      role: {
+        name: '',
+        nameZh: ''
+      },
+      roles: [],
+      allMenus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  mounted() {
+    this.getAllRolesData();
+  },
+  methods: {
+    changeCollapse(rid) {
+      if (rid) {
+        this.getAllMenus();
+      }
+    },
+    getAllMenus() {
+      this.getRequest('/system/basic/permission/menus').then(response => {
+        if (response) {
+          this.allMenus = response;
+        }
+      });
+    },
+    getAllRolesData() {
+      this.getRequest('/system/basic/permission/').then(response => {
+        if (response) {
+          this.roles = response;
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+.permissManaTool {
+  display: flex;
+  justify-content: flex-start;
+}
 
+.permissManaTool .el-input {
+  width: 300px;
+  margin-right: 8px;
+}
+
+.permissManaMain {
+  margin-top: 10px;
+  width: 700px;
+}
 </style>
--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -4,7 +4,7 @@
       <el-tab-pane label="部门管理" name="DeptMana"><dept-mana></dept-mana></el-tab-pane>
       <el-tab-pane label="职位管理" name="PosMana"><pos-mana></pos-mana></el-tab-pane>
       <el-tab-pane label="职称管理" name="JobLevelMana"><job-level-mana></job-level-mana></el-tab-pane>
-      <el-tab-pane label="职称管理" name="EcMana"><ec-mana></ec-mana></el-tab-pane>
+      <el-tab-pane label="奖惩规则" name="EcMana"><ec-mana></ec-mana></el-tab-pane>
       <el-tab-pane label="权限组" name="PermissMana"><permiss-mana></permiss-mana></el-tab-pane>
     </el-tabs>
   </div>
@@ -21,7 +21,7 @@ export default {
   name: "SysBasic",
   data() {
     return {
-      activeName: 'JobLevelMana'
+      activeName: 'PermissMana'
     }
   },
   components: {
