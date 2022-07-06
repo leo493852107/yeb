8f4c1a5813f2dbd294578dae17c585e4ebb47c04
leo <leo493852107@163.com>
2022-07-06 21:01:50 +0800

    员工资料工具栏页

--- src/views/emp/EmpBasic.vue
+++ src/views/emp/EmpBasic.vue
@@ -1,5 +1,29 @@
 <template>
-<div>EmpBasic</div>
+  <div>
+    <div style="display: flex; justify-content: space-between">
+      <div>
+        <el-input style="width: 300px;margin-right: 10px" prefix-icon="el-icon-search"
+                  placeholder="请输入员工名进行搜索.."></el-input>
+        <el-button type="primary" prefix-icon="el-icon-search">搜索</el-button>
+        <el-button type="primary">
+          <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+          高级搜索
+        </el-button>
+      </div>
+      <div>
+        <el-button type="success">
+          <i class="fa fa-level-up" aria-hidden="true"></i>
+          导入数据
+        </el-button>
+        <el-button type="success">
+          <i class="fa fa-level-down" aria-hidden="true"></i>
+          导出数据
+        </el-button>
+        <el-button type="primary" icon="el-icon-plus">添加员工</el-button>
+      </div>
+    </div>
+    <div></div>
+  </div>
 </template>
 
 <script>
--- src/views/sys/SysAdmin.vue
+++ src/views/sys/SysAdmin.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <div style="display: flex; justify-content: center; margin-top: 10px">
-      <el-input placeholder="用户名搜索用户" prefix-icon="el-icon-search" style="width: 400px; margin-right: 10px"></el-input>
-      <el-button icon="el-icon-search" type="primary">搜索</el-button>
+      <el-input v-model="keywords" placeholder="用户名搜索用户" prefix-icon="el-icon-search"
+                style="width: 400px; margin-right: 10px"></el-input>
+      <el-button icon="el-icon-search" type="primary" @click="doSearch">搜索</el-button>
     </div>
     <div class="admin-container">
       <el-card class="admin-card" v-for="(admin, index) in admins" :key="index">
         <div slot="header" class="clearfix">
           <span>{{ admin.name }}</span>
-          <el-button style="float: right; padding: 3px 0; color: red" type="text" icon="el-icon-delete"></el-button>
+          <el-button style="float: right; padding: 3px 0; color: red" type="text" icon="el-icon-delete"
+                     @click="deleteAdmin(admin)"></el-button>
         </div>
         <div>
           <div class="img-container">
@@ -27,6 +29,7 @@
                 inactive-color="#ff4949"
                 active-text="启用"
                 inactive-text="禁用"
+                @change="enabledChange(admin)"
             >
             </el-switch>
           </div>
@@ -35,7 +38,23 @@
             <el-tag style="margin-right: 10px" type="success" v-for="(role, index) in admin.roles" :key="index">
               {{ role.nameZh }}
             </el-tag>
-            <el-button type="text" icon="el-icon-more"></el-button>
+            <el-popover
+                placement="right"
+                title="角色列表"
+                width="200"
+                @show="showPop(admin)"
+                @hide="hidePop(admin)"
+                trigger="click">
+              <el-select v-model="selectRoles" multiple placeholder="请选择">
+                <el-option
+                    v-for="(r, index) in allRoles"
+                    :key="index"
+                    :label="r.nameZh"
+                    :value="r.id">
+                </el-option>
+              </el-select>
+              <el-button slot="reference" type="text" icon="el-icon-more"></el-button>
+            </el-popover>
           </div>
           <div>
             备注: {{ admin.remark }}
@@ -51,19 +70,99 @@ export default {
   name: "SysAdmin",
   data() {
     return {
-      admins: []
+      admins: [],
+      keywords: '',
+      allRoles: [],
+      selectRoles: []
     }
   },
   mounted() {
     this.getAdmins();
   },
   methods: {
+    hidePop(admin) {
+      let roles = [];
+      Object.assign(roles, admin.roles);
+      let flag = false;
+      if (roles.length !== this.selectRoles.length) {
+        flag = true;
+      } else {
+        for (let i = 0; i < roles.length; i++) {
+          let role = roles[i];
+          for (let j = 0; j < this.selectRoles.length; j++) {
+            let sr = this.selectRoles[j];
+            if (role.id === sr) {
+              roles.splice(i, 1);
+              i--;
+              break;
+            }
+          }
+        }
+        if (roles.length !== 0) {
+          flag = true;
+        }
+      }
+      if (flag) {
+        let url = '/system/admin/role?adminId=' + admin.id;
+        this.selectRoles.forEach(item => {
+          url += '&rids=' + item;
+        });
+        this.putRequest(url).then(resp => {
+          if (resp) {
+            this.getAdmins();
+          }
+        })
+      }
+    },
+    showPop(admin) {
+      this.initAllRoles();
+      let roles = admin.roles;
+      this.selectRoles = [];
+      roles.forEach(r => {
+        this.selectRoles.push(r.id);
+      });
+    },
+    initAllRoles() {
+      this.getRequest('/system/admin/roles').then(resp => {
+        if (resp) {
+          this.allRoles = resp;
+        }
+      })
+    },
+    enabledChange(admin) {
+      this.putRequest('/system/admin/', admin).then(resp => {
+        if (resp) {
+          this.getAdmins();
+        }
+      })
+    },
     getAdmins() {
-      this.getRequest('/system/admin/').then(response => {
+      this.getRequest('/system/admin/?keywords=' + this.keywords).then(response => {
         if (response) {
           this.admins = response;
         }
       })
+    },
+    doSearch() {
+      this.getAdmins();
+    },
+    deleteAdmin(admin) {
+      this.$confirm('此操作将永久删除[' + admin.name + '], 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest('/system/admin/' + admin.id).then(response => {
+          if (response) {
+            this.getAdmins();
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     }
   }
 }
