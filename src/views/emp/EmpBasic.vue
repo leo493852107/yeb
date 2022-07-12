050ea94db95acb1feb3cfd62344c6d131c43be87
leo <leo493852107@163.com>
2022-07-12 20:51:40 +0800

    前端员工展示

--- src/views/emp/EmpBasic.vue
+++ src/views/emp/EmpBasic.vue
@@ -22,13 +22,212 @@
         <el-button type="primary" icon="el-icon-plus">添加员工</el-button>
       </div>
     </div>
-    <div></div>
+    <div style="margin-top: 10px">
+      <el-table
+          :data="emps"
+          stripe
+          border
+          v-loading="loading"
+          element-loading-text="拼命加载中"
+          element-loading-spinner="el-icon-loading"
+          element-loading-background="rgba(0, 0, 0, 0.8)"
+          style="width: 100%">
+        <el-table-column
+            type="selection"
+            width="55">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="姓名"
+            fixed
+            align="left"
+            width="90">
+        </el-table-column>
+        <el-table-column
+            prop="workID"
+            label="工号"
+            align="left"
+            width="85">
+        </el-table-column>
+        <el-table-column
+            prop="gender"
+            label="性别"
+            align="left"
+            width="50">
+        </el-table-column>
+        <el-table-column
+            prop="birthday"
+            label="出生日期"
+            align="left"
+            width="95">
+        </el-table-column>
+        <el-table-column
+            prop="idCard"
+            label="身份证"
+            align="left"
+            width="140">
+        </el-table-column>
+        <el-table-column
+            prop="wedlock"
+            label="婚姻情况"
+            align="left"
+            width="85">
+        </el-table-column>
+        <el-table-column
+            prop="nation.name"
+            label="民族"
+            align="left"
+            width="50">
+        </el-table-column>
+        <el-table-column
+            prop="nativePlace"
+            label="籍贯"
+            align="left"
+            width="80">
+        </el-table-column>
+        <el-table-column
+            prop="politicsStatus.name"
+            label="政治面貌"
+            align="left"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            prop="email"
+            label="email"
+            align="left"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="phone"
+            label="电话"
+            align="left"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            prop="address"
+            label="地址"
+            align="left"
+            width="270">
+        </el-table-column>
+        <el-table-column
+            prop="department.name"
+            label="部门"
+            align="left"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            prop="joblevel.name"
+            label="职称"
+            align="left"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            prop="position.name"
+            label="职位"
+            align="left"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            prop="engageForm"
+            label="聘用方式"
+            align="left"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            prop="tiptopDegree"
+            label="最高学历"
+            align="left"
+            width="80">
+        </el-table-column>
+        <el-table-column
+            prop="school"
+            label="学校"
+            align="left"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="specialty"
+            label="专业"
+            align="left"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="workState"
+            label="在职装填"
+            align="left"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="beginDate"
+            label="入职日期"
+            align="left"
+            width="95">
+        </el-table-column>
+        <el-table-column
+            prop="conversionTime"
+            label="转正日期"
+            align="left"
+            width="95">
+        </el-table-column>
+        <el-table-column
+            prop="beginContract"
+            label="合同开始日期"
+            align="left"
+            width="85">
+        </el-table-column>
+        <el-table-column
+            prop="endContract"
+            label="合同截止日期"
+            align="left"
+            width="95">
+        </el-table-column>
+        <el-table-column
+            label="合同期限"
+            align="left"
+            width="100">
+          <template slot-scope="scope">
+            <el-tag>{{ scope.row.contractTerm }}</el-tag>
+            年
+          </template>
+        </el-table-column>
+        <el-table-column
+            label="操作"
+            fixed="right"
+            width="200">
+          <template slot-scope="scope">
+            <el-button style="padding: 3px" size="mini">编辑</el-button>
+            <el-button style="padding: 3px" size="mini" type="primary">查看高级资料</el-button>
+            <el-button style="padding: 3px" size="mini" type="danger">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "EmpBasic"
+  name: "EmpBasic",
+  data() {
+    return {
+      emps: [],
+      loading: false
+    }
+  },
+  mounted() {
+    this.getEmps();
+  },
+  methods: {
+    getEmps() {
+      this.loading = true;
+      this.getRequest('/employee/basic/').then(resp => {
+        this.loading = false;
+        if (resp) {
+          this.emps = resp.data;
+        }
+      })
+    }
+  }
 }
 </script>
 
